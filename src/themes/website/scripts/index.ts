3b0c4f81f162f92c989bafdc92b6062601982ad7
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-03-02 21:33:16 -0800

    tmp

--- src/themes/website/scripts/index.ts
+++ src/themes/website/scripts/index.ts
@@ -7,3 +7,4 @@
  */
 
 import "./collapsibles";
+import "./intersections";
--- src/themes/website/scripts/intersections.ts
+++ src/themes/website/scripts/intersections.ts
@@ -1,32 +1,42 @@
-const scrollIntersections = document.querySelectorAll(`[class="scroll"],[class^="scroll-"]`);
 
-const callback: IntersectionObserverCallback = (entries, observer) => {
-    console.log(entries);
 
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
+const initialize = () => {
+    const scrollIntersections = document.querySelectorAll(`.button`);
+
+    // ,[class="button"],[class^="scroll-"]
+
+    const callback: IntersectionObserverCallback = (entries, observer) => {
+        console.log(entries);
+
+        entries.forEach(entry => {
             // cases: once, while-in-view, while-meets-scroll-position
-        }
+
+            if (!entry.isIntersecting) {
+                return;
+            }
+
+            const classes = Array.prototype.slice.call(entry.target.classList);
+            // const scrollPointerClass = classes.find(c => c.startsWith("scroll-"));
+
+            const scrollClass = classes.find(c => c === "button");
+
+            if (scrollClass) {
+                entry.target.classList.add("scroll-active");
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(callback, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.25
+    });
+
+    scrollIntersections.forEach(x => {
+        observer.observe(x);
+        console.log(x);
     });
 };
 
-const observer = new IntersectionObserver(callback, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.25
-});
+setTimeout(initialize, 500);
 
-scrollIntersections.forEach(x => {
-    observer.observe(x);
-
-    // const classes = Array.prototype.slice.call(x.classList);
-    // const scrollPointerClass = classes.find(c => c.startsWith("scroll-"));
-
-
-
-    // const scrollClass = classes.find(c => c.startsWith("scroll"));
-    // if (scrollClass) {
-
-    // }
-
-});
